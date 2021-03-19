bd6d1daab3efc7b50366a83a86539e7b1a6266a4
sharphurt <pavellukoanov679@gmail.com>
2021-03-19 19:20:53 +0500

    Initial commit
    Recreated repository
    Organized files/dirs
    Added screen borders. Now the car cannot go outside the visible area

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    public Joystick joystick;
+
+    public float acceleration;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        var size = boxCollider2D.size;
+    }
+
+
+    void FixedUpdate()
+    {
+        var screenEdges = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        var boundsHalfSize = boxCollider2D.bounds.size / 2f;
+        rb.position = new Vector2(
+            Mathf.Clamp(rb.position.x, -screenEdges.x + boundsHalfSize.x, screenEdges.x - boundsHalfSize.x),
+            Mathf.Clamp(rb.position.y, -screenEdges.y + boundsHalfSize.y, screenEdges.y - boundsHalfSize.y));
+
+        rb.velocity = new Vector2(joystick.Horizontal * acceleration + Input.GetAxis("Horizontal") * acceleration,
+            joystick.Vertical * acceleration + Input.GetAxis("Vertical") * acceleration);
+
+        rb.rotation = Mathf.Asin(joystick.Horizontal) * -Mathf.PI * acceleration;
+    }
+}
