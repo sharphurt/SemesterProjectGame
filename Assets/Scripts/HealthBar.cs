96417d6e8ee28189eb1e9fcb6d061f9c294d9315
sharphurt <pavellukoanov679@gmail.com>
2021-03-21 04:47:27 +0500

    adds to prev commit

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar;
+    private int currentValue;
+    private int reachingHealth;
+    private int maxHealth;
+
+    private void Update()
+    {
+        if (currentValue != reachingHealth)
+        {
+            currentValue = (int) Math.Round(Mathf.Lerp(currentValue, reachingHealth, 1.5f * Time.deltaTime));
+            healthBar.fillAmount = 1f / maxHealth * currentValue;
+        }
+    }
+
+    public void SetHealthBar(int value, int max, bool isInstantly)
+    {
+        reachingHealth = value;
+        maxHealth = max;
+        if (isInstantly)
+            currentValue = reachingHealth;
+    }
+}
