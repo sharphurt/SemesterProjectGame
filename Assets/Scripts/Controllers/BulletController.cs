cf1521d58c9a538cb782de8bd675e1f75f25c2f1
sharphurt <pavellukoanov679@gmail.com>
2021-03-22 03:39:10 +0500

    Code refactoring

--- Assets/Scripts/Controllers/BulletController.cs
+++ Assets/Scripts/Controllers/BulletController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Entities;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
--- Assets/Scripts/LocationMethod.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-public enum LocationMethod
-{
-    [EnumMember(Value = "random")] Random,
-    [EnumMember(Value = "specified")] Specified
-}
