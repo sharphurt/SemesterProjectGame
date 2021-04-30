52bd29037350a29f85fa7d0fb15b0b38498e5ea1
sharphurt <pavellukoanov679@gmail.com>
2021-05-01 00:16:53 +0500

    Updated textures, fixed bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -82,7 +82,7 @@ public class GameManager : MonoBehaviour
         var instance = Instantiate(LevelEndPointPrefab, LevelData.levelEnd.spawnPosition, Quaternion.identity);
         instance.OnGameWin += GameWinHandler;
         player.DisableJoystickControl();
-        player.MoveTo(instance.GetComponentInChildren<Transform>().position, 0.3f, true);
+        player.MoveTo(instance.GetComponentInChildren<Transform>().position, 0.1f, true);
         isFinishing = true;
 
     }
